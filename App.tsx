bab6d15f5b188f7dfe46a09d1493821dc1c08e7c
alex.nguyen <alex.nguyen@applifting.cz>
2021-06-26 16:34:38 +0200

    First commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera, FaceDetectionResult, PermissionStatus } from 'expo-camera';
+import * as FaceDetector from 'expo-face-detector';
+import { Face } from 'expo-camera/build/Camera.types';
+
+const faceDetectorSettings = {
+  mode: FaceDetector.Constants.Mode.accurate,
+  detectLandmarks: FaceDetector.Constants.Landmarks.none,
+  minDetectionInterval: 1000,
+};
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(false);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [faceRes, setFaceRes] = useState<Face | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === PermissionStatus.GRANTED);
+    })();
+  }, []);
+
+  const handleFlip = () => setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back);
+
+  const handleFacesDetected = (result: FaceDetectionResult) => {
+    // console.log('its a face');
+    setFaceRes(result.faces[0]);
+  };
+
+  const boxStyle = () => ({
+    width: faceRes?.bounds.size.width,
+    height: faceRes?.bounds.size.height,
+    left: faceRes?.bounds.origin.x,
+    top: faceRes?.bounds.origin.y,
+    position: 'absolute',
+    borderColor: 'blue',
+    borderWidth: 5,
+  });
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={type} onFacesDetected={handleFacesDetected} faceDetectorSettings={faceDetectorSettings}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={handleFlip}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+          {/* @ts-ignore */}
+          {faceRes && <View style={boxStyle()}></View>}
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -14,8 +65,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+  },
+  button: {
+    padding: 10,
+    backgroundColor: 'black',
+    alignSelf: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
   },
 });
