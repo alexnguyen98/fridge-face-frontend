039b9c08912cbb2e6b424ad7e40201fe1e835e0b
alex.nguyen <alex.nguyen@applifting.cz>
2021-07-12 22:34:31 +0200

    update

--- App.tsx
+++ App.tsx
@@ -1,87 +1,115 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { Camera, FaceDetectionResult, PermissionStatus } from 'expo-camera';
-import * as FaceDetector from 'expo-face-detector';
-import { Face } from 'expo-camera/build/Camera.types';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
 
-const faceDetectorSettings = {
-  mode: FaceDetector.Constants.Mode.accurate,
-  detectLandmarks: FaceDetector.Constants.Landmarks.none,
-  minDetectionInterval: 1000,
-};
+ import React from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+ } from 'react-native';
 
-export default function App() {
-  const [hasPermission, setHasPermission] = useState(false);
-  const [type, setType] = useState(Camera.Constants.Type.back);
-  const [faceRes, setFaceRes] = useState<Face | null>(null);
+ import {
+   Colors,
+   DebugInstructions,
+   Header,
+   LearnMoreLinks,
+   ReloadInstructions,
+ } from 'react-native/Libraries/NewAppScreen';
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === PermissionStatus.GRANTED);
-    })();
-  }, []);
+ const Section: React.FC<{
+   title: string;
+ }> = ({children, title}) => {
+   const isDarkMode = useColorScheme() === 'dark';
+   return (
+     <View style={styles.sectionContainer}>
+       <Text
+         style={[
+           styles.sectionTitle,
+           {
+             color: isDarkMode ? Colors.white : Colors.black,
+           },
+         ]}>
+         {title}
+       </Text>
+       <Text
+         style={[
+           styles.sectionDescription,
+           {
+             color: isDarkMode ? Colors.light : Colors.dark,
+           },
+         ]}>
+         {children}
+       </Text>
+     </View>
+   );
+ };
 
-  const handleFlip = () => setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back);
+ const App = () => {
+   const isDarkMode = useColorScheme() === 'dark';
 
-  const handleFacesDetected = (result: FaceDetectionResult) => {
-    const data = result.faces[0];
-    setFaceRes(data);
-  };
+   const backgroundStyle = {
+     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+   };
 
-  const boxStyle = () => ({
-    width: faceRes?.bounds.size.width,
-    height: faceRes?.bounds.size.height,
-    left: faceRes?.bounds.origin.x,
-    top: faceRes?.bounds.origin.y,
-    position: 'absolute',
-    borderColor: 'blue',
-    borderWidth: 5,
-  });
+   return (
+     <SafeAreaView style={backgroundStyle}>
+       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+       <ScrollView
+         contentInsetAdjustmentBehavior="automatic"
+         style={backgroundStyle}>
+         <Header />
+         <View
+           style={{
+             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+           }}>
+           <Section title="Step One">
+             Edit <Text style={styles.highlight}>App.js</Text> to change this
+             screen and then come back to see your edits.
+           </Section>
+           <Section title="See Your Changes">
+             <ReloadInstructions />
+           </Section>
+           <Section title="Debug">
+             <DebugInstructions />
+           </Section>
+           <Section title="Learn More">
+             Read the docs to discover what to do next:
+           </Section>
+           <LearnMoreLinks />
+         </View>
+       </ScrollView>
+     </SafeAreaView>
+   );
+ };
 
-  if (hasPermission === null) {
-    return <View />;
-  }
+ const styles = StyleSheet.create({
+   sectionContainer: {
+     marginTop: 32,
+     paddingHorizontal: 24,
+   },
+   sectionTitle: {
+     fontSize: 24,
+     fontWeight: '600',
+   },
+   sectionDescription: {
+     marginTop: 8,
+     fontSize: 18,
+     fontWeight: '400',
+   },
+   highlight: {
+     fontWeight: '700',
+   },
+ });
 
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
-
-  return (
-    <View style={styles.container}>
-      <Camera style={styles.camera} type={type} onFacesDetected={handleFacesDetected} faceDetectorSettings={faceDetectorSettings}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={handleFlip}>
-            <Text style={styles.text}> Flip </Text>
-          </TouchableOpacity>
-          {/* @ts-ignore */}
-          {faceRes && <View style={boxStyle()}></View>}
-        </View>
-      </Camera>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
-  },
-  button: {
-    padding: 10,
-    backgroundColor: 'black',
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 18,
-    color: 'white',
-  },
-});
+ export default App;
