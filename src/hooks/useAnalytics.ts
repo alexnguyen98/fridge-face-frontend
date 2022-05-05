053f390d880b72f727f5ea0c547ef2a54a04ed17
alex.nguyen <alex.nguyen@applifting.cz>
2022-05-05 14:25:42 +0200

    update

--- src/hooks/useAnalytics.ts
+++ src/hooks/useAnalytics.ts
@@ -2,8 +2,12 @@ import * as Analytics from 'expo-firebase-analytics';
 
 export const useAnalytics = () => {
   const logEvent = async (name: string, data?: any) => {
-    await Analytics.setDebugModeEnabled(false);
-    await Analytics.logEvent(name, data);
+    try {
+      await Analytics.setDebugModeEnabled(false);
+      await Analytics.logEvent(name, data);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return { logEvent };
