aa5cbe469890442e27494d97cd96f073752cd00b
alex.nguyen <alex.nguyen@applifting.cz>
2022-05-16 15:33:25 +0200

    update

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 // export const SERVER_URL = 'http://10.0.1.37:3000'; // House
 // export const SERVER_URL = 'http://192.168.3.176:3000'; // Applifting
-export const SERVER_URL = 'http://10.0.2.97:3000'; // CVUT
+export const SERVER_URL = 'http://10.0.0.68:3000'; // CVUT
 // export const SERVER_URL = 'http://10.4.115.172:3000'; // NTK
 // export const SERVER_URL = 'https://nguyexu.tech'; // Production domain
 // export const SERVER_URL = 'http://159.223.27.234:3000'; // Production ip
